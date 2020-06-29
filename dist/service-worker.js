dc842543410d0b0a05801d3e18d8ace15c9b6766
trevorkennedy <direct@trevorkennedy.com>
2020-06-29 01:53:32 -0500

    netlify

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -12,7 +12,7 @@
  */
 
 importScripts(
-  "/progressive-weather-app/precache-manifest.ab644820230c5211906cf74d93f4a4a3.js",
+  "/progressive-weather-app/precache-manifest.04d35cbbd22ccdd7cb50c140e4baa624.js",
   "https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js"
 );
 
